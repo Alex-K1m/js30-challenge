cd949cad378182b137d90a5dc681b068bcd0f2a8
Alex Kim <henvin.3@outlook.com>
2019-04-19 21:58:14 +0500

    add animation

--- challenges/01 - JavaScript Drum Kit/script.js
+++ challenges/01 - JavaScript Drum Kit/script.js
@@ -6,8 +6,10 @@ const keyCodesInUse = audioElements.map(el => el.dataset.key);
 document.addEventListener('keypress', ({ key }) => {
   const keyCode = key.toUpperCase().charCodeAt();
   if (!keyCodesInUse.includes(keyCode.toString())) return;
-  document.querySelector(`audio[data-key="${keyCode}"]`).play();
-});
 
-// TODO: animate
-// const buttons = document.querySelectorAll('.key');
+  document.querySelector(`audio[data-key="${keyCode}"]`).play();
+
+  const button = document.querySelector(`div[data-key="${keyCode}"]`);
+  button.classList.add('playing');
+  setTimeout(() => button.classList.remove('playing'), 70);
+});
