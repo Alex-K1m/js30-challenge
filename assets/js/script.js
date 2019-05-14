7092aef7370ccecc5d727acfba065c43034337c4
Alex <henvin.3@outlook.com>
2019-05-14 16:08:52 +0500

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const canvas = /** @type {HTMLCanvasElement} */ (document.querySelector('canvas.background'));
-const { innerWidth } = window;
-const { innerHeight } = window;
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+const width = window.innerWidth;
+const height = window.innerHeight;
+canvas.width = width;
+canvas.height = height;
 const c = canvas.getContext('2d');
+const numOfCircles = 8;
 
 const genRandomInt = (min = 0, max = 1) => Math.round(
   Math.random() * (max - min) + min,
@@ -14,8 +15,8 @@ const genRandomInt = (min = 0, max = 1) => Math.round(
 class Circle {
   constructor() {
     this.init();
-    this.x = genRandomInt(-200, innerWidth);
-    this.y = genRandomInt(-200, innerHeight);
+    this.x = genRandomInt(-200, width);
+    this.y = genRandomInt(-200, height);
   }
 
   init() {
@@ -25,16 +26,16 @@ class Circle {
     this.dy = delta;
     if (genRandomInt()) {
       this.x = -1 * this.radius;
-      this.y = genRandomInt(0, innerHeight - 2 * this.radius);
+      this.y = genRandomInt(0, height - 2 * this.radius);
     } else {
-      this.x = genRandomInt(0, innerWidth - 2 * this.radius);
+      this.x = genRandomInt(0, width - 2 * this.radius);
       this.y = -1 * this.radius;
     }
   }
 
   update() {
-    if (this.x - this.radius > innerWidth
-    || this.y - this.radius > innerHeight) {
+    if (this.x - this.radius > width
+    || this.y - this.radius > height) {
       this.init();
     }
     this.x += this.dx;
@@ -50,12 +51,12 @@ class Circle {
   }
 }
 
-const circles = new Array(8)
+const circles = new Array(numOfCircles)
   .fill(null)
   .map(() => new Circle());
 
 const animate = () => {
-  c.clearRect(0, 0, innerWidth, innerHeight);
+  c.clearRect(0, 0, width, height);
   circles.forEach(circle => circle.draw());
   requestAnimationFrame(animate);
 };
