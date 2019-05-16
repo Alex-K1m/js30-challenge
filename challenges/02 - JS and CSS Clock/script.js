002f00debe60192b80f76964c12998f519316515
Alex <henvin.3@outlook.com>
2019-05-16 21:09:04 +0500

    clock prototype

--- challenges/02 - JS and CSS Clock/script.js
+++ challenges/02 - JS and CSS Clock/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const secToDeg = sec => sec * 6; // 360 deg / 60 sec
+
+const initClockHand = (hand, current, step) => {
+  let degree = 90 + current; // +90deg to fix initial direction of the clock hands
+  const setPosition = () => { hand.style.transform = `rotate(${degree}deg)`; };
+  setPosition();
+
+  return () => {
+    degree = (degree % 360) + step;
+    setPosition();
+  };
+};
+
+const secondHand = document.querySelector('.second-hand');
+
+const time = new Date(Date.now());
+const sec = time.getSeconds();
+const ms = time.getMilliseconds();
+
+const tick = initClockHand(secondHand, secToDeg(sec), secToDeg(1));
+
+const syncTick = () => {
+  tick();
+  setInterval(tick, 1000);
+};
+
+setTimeout(syncTick, 1000 - ms);
