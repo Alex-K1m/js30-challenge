b5bcecbc3f2124d01d29445334ce167f08fba974
Alex <henvin.3@outlook.com>
2019-05-20 21:28:11 +0500

    clock solution 2

--- challenges/02 - JS and CSS Clock/script.js
+++ challenges/02 - JS and CSS Clock/script.js
@@ -1,39 +1,28 @@
 'use strict';
 
-const secToDeg = sec => sec * 6; // 360 deg / 60 sec
-const minSecToDeg = (min, sec) => min * 6 + Math.floor(sec / 10);
-const hourMinToDeg = (hour, min) => (hour % 12) * 30 + Math.floor(min / 2);
-
-const initClockHand = (hand, current, step) => {
-  let degree = 90 + current; // +90deg to fix initial direction of the clock hands
-  const setPosition = () => { hand.style.transform = `rotate(${degree}deg)`; };
-  setPosition();
-
-  return () => {
-    degree = (degree % 360) + step;
-    setPosition();
-  };
-};
-
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
-const time = new Date(Date.now());
-const sec = time.getSeconds();
-const min = time.getMinutes();
-const hour = time.getHours();
-const now = Date.now();
+const update = () => {
+  const time = new Date();
+  const sec = time.getSeconds();
+  const min = time.getMinutes();
+  const hour = time.getHours();
 
-const tickSec = initClockHand(secondHand, secToDeg(sec), 6);
-const tickMin = initClockHand(minuteHand, minSecToDeg(min, sec), 1);
-const tickHour = initClockHand(hourHand, hourMinToDeg(hour, min), 1);
+  const secDegrees = sec * 6 + 90;
+  const minDegrees = min * 6 + sec / 10 + 90;
+  const hourDegrees = (hour % 12) * 30 + min / 2 + 90;
 
-const sync = (tick, interval) => () => {
-  tick();
-  setInterval(tick, interval);
+  secondHand.style.transform = `rotate(${secDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 };
 
-setTimeout(sync(tickSec, 1000), 1000 - (now % 1000));
-setTimeout(sync(tickMin, 10000), 10000 - (now % 10000));
-setTimeout(sync(tickHour, 120000), 120000 - (now % 120000));
+const sync = () => {
+  update();
+  setInterval(update, 1000);
+};
+
+update();
+setTimeout(sync, 1000 - (Date.now() % 1000));
