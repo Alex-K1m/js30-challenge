f9786ec036b1a7d0fbc28ffd639d6a2008733c71
Alex <henvin.3@outlook.com>
2019-05-21 17:24:22 +0500

    css variables solution

--- challenges/03 - CSS Variables/script.js
+++ challenges/03 - CSS Variables/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const image = document.querySelector('img');
+
+const update = ({ target: { id, value } }) => {
+  image.style.setProperty(`--${id}`, value[0] === '#' ? value : `${value}px`);
+};
+
+['spacing', 'blur', 'base']
+  .map(id => document.getElementById(id))
+  .forEach(input => input.addEventListener('input', update));
