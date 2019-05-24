07caf4d6d39d5324c86ea558e7574527709dc1a0
Alex <henvin.3@outlook.com>
2019-05-24 16:20:12 +0500

    type ahead draft

--- challenges/06 - Type Ahead/script.js
+++ challenges/06 - Type Ahead/script.js
@@ -1,3 +1,37 @@
 'use strict';
 
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const getData = async () => {
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  return data;
+};
+
+const initialUl = document.querySelector('.suggestions').cloneNode(true);
+
+getData().then((data) => {
+  document
+    .querySelector('.search')
+    .addEventListener('input', ({ target }) => {
+      if (target.value === '') {
+        document
+          .querySelector('.suggestions')
+          .replaceWith(initialUl);
+        return;
+      }
+      const list = data
+        .filter(({ city }) => city.includes(target.value))
+        .map(({ city }) => {
+          const li = document.createElement('li');
+          li.textContent = city;
+          return li;
+        });
+      const ul = document.createElement('ul');
+      ul.classList.add('suggestions');
+      ul.append(...list);
+      document
+        .querySelector('.suggestions')
+        .replaceWith(ul);
+    });
+});
