70bf7ae7d57c8ac68162145d0412ece73cccbd3c
Alex Kim <henvin.3@outlook.com>
2019-05-31 19:46:48 +0500

    canvas add basic drawing logic

--- challenges/08 - Fun with HTML5 Canvas/script.js
+++ challenges/08 - Fun with HTML5 Canvas/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById('draw'));
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const c = canvas.getContext('2d');
+
+let mouseDown = false;
+
+const drawCircle = (x, y, r) => {
+  c.beginPath();
+  c.arc(x, y, r, 0, Math.PI * 2);
+  c.fillStyle = 'Dodger Blue';
+  c.fill();
+};
+
+const paint = ({ x, y }) => {
+  if (mouseDown) {
+    drawCircle(x, y, 20);
+  }
+};
+
+canvas.addEventListener('mousedown', (e) => { mouseDown = true; paint(e); });
+canvas.addEventListener('mouseup', () => { mouseDown = false; });
+canvas.addEventListener('mouseleave', () => { mouseDown = false; });
+canvas.addEventListener('mousemove', paint);
