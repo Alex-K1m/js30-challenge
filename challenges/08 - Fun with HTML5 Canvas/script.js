dc82b3f301981dcf36d26e87a7036c9ad5b5b9f6
Alex Kim <henvin.3@outlook.com>
2019-06-02 23:50:53 +0500

    canvas reimplement drawing using lines

--- challenges/08 - Fun with HTML5 Canvas/script.js
+++ challenges/08 - Fun with HTML5 Canvas/script.js
@@ -7,33 +7,28 @@ const c = canvas.getContext('2d');
 
 let mouseDown = false;
 let last = {};
-const r = 20;
+c.strokeStyle = 'gray';
+c.lineJoin = 'round';
+c.lineCap = 'round';
+c.lineWidth = 20;
 
-const paintSegment = ({ x, y }) => {
+const paint = ({ x, y }) => {
   if (!mouseDown) return;
   c.beginPath();
-  const angle = Math.atan2(last.y - y, last.x - x);
-  const a1 = angle - Math.PI / 2;
-  const a2 = angle + Math.PI / 2;
-  c.arc(last.x, last.y, r, a1, a2);
-  c.arc(x, y, r, a2, a1);
-  c.fillStyle = 'gray';
-  c.fill();
+  c.moveTo(last.x, last.y);
+  c.lineTo(x, y);
+  c.stroke();
   last = { x, y };
 };
 
-const paintCircle = ({ x, y }) => {
-  c.beginPath();
-  c.arc(x, y, r, 0, Math.PI * 2);
-  c.fillStyle = 'gray';
-  c.fill();
-  last = { x, y };
+const setMouseDown = ({ x, y }) => {
   mouseDown = true;
+  last = { x, y };
 };
 
 const unsetMouseDown = () => { mouseDown = false; };
 
-canvas.addEventListener('mousedown', paintCircle);
+canvas.addEventListener('mousedown', setMouseDown);
 canvas.addEventListener('mouseup', unsetMouseDown);
 canvas.addEventListener('mouseleave', unsetMouseDown);
-canvas.addEventListener('mousemove', paintSegment);
+canvas.addEventListener('mousemove', paint);
