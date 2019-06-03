7b62478a720fc20932aafd1f48d9e5d429ed554d
Alex Kim <henvin.3@outlook.com>
2019-06-03 21:50:47 +0500

    canvas refactor

--- challenges/08 - Fun with HTML5 Canvas/script.js
+++ challenges/08 - Fun with HTML5 Canvas/script.js
@@ -3,41 +3,70 @@
 const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById('draw'));
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const c = canvas.getContext('2d');
+const context = canvas.getContext('2d');
 
-let mouseDown = false;
-let last = {};
-let hue = 0;
-let size = 6;
-let delta = 1;
+function makeDrawingEvent(element) {
+  let mouseDown = false;
+  let recipients = [];
 
-c.lineJoin = 'round';
-c.lineCap = 'round';
+  const setMouseDown = () => { mouseDown = true; };
 
-const paint = ({ x, y }) => {
-  if (!mouseDown) return;
-  c.beginPath();
-  c.moveTo(last.x, last.y);
-  c.lineTo(x, y);
-  c.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-  c.lineWidth = size;
-  c.stroke();
-  last = { x, y };
-  hue = (hue + 1) % 360;
-  if (size <= 5 || size >= 60) {
-    delta *= -1;
-  }
-  size += delta;
-};
+  const unsetMouseDown = () => { mouseDown = false; };
 
-const setMouseDown = ({ x, y }) => {
-  mouseDown = true;
-  last = { x, y };
-};
+  const dispatch = (e) => {
+    if (!mouseDown) return;
+    recipients.forEach(async rec => rec(e));
+  };
 
-const unsetMouseDown = () => { mouseDown = false; };
+  const subscribe = (...rest) => {
+    recipients = [...recipients, ...rest];
+  };
 
-canvas.addEventListener('mousedown', setMouseDown);
-canvas.addEventListener('mouseup', unsetMouseDown);
-canvas.addEventListener('mouseleave', unsetMouseDown);
-canvas.addEventListener('mousemove', paint);
+  const unSubscribe = (...rest) => {
+    recipients = recipients.filter(rec => !rest.includes(rec));
+  };
+
+  element.addEventListener('mousedown', setMouseDown);
+  element.addEventListener('mouseup', unsetMouseDown);
+  element.addEventListener('mouseleave', unsetMouseDown);
+  element.addEventListener('mousemove', dispatch);
+
+  return { subscribe, unSubscribe };
+}
+
+function makeBrush(c) {
+  let last = {};
+  let hue = 0;
+  let size = 6;
+  let delta = 1;
+
+  const paint = ({ x, y }) => {
+    c.beginPath();
+    c.moveTo(last.x, last.y);
+    c.lineTo(x, y);
+
+    c.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    c.lineWidth = size;
+    c.lineJoin = 'round';
+    c.lineCap = 'round';
+    c.stroke();
+
+    last = { x, y };
+    hue = (hue + 1) % 360;
+    if (size <= 5 || size >= 60) {
+      delta *= -1;
+    }
+    size += delta;
+  };
+
+  const moveTo = ({ x, y }) => {
+    last = { x, y };
+  };
+
+  return { paint, moveTo };
+}
+
+const brush = makeBrush(context);
+const drawingEvent = makeDrawingEvent(canvas);
+drawingEvent.subscribe(brush.paint);
+canvas.addEventListener('mousedown', brush.moveTo);
