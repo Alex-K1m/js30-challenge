c42118b1269b1bc2332f55ede0c7067124c74a25
Alex Kim <henvin.3@outlook.com>
2019-06-10 18:03:10 +0500

    local storage add logic for adding items to list

--- challenges/15 - LocalStorage/script.js
+++ challenges/15 - LocalStorage/script.js
@@ -2,4 +2,40 @@
 
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const state = { items: [] };
+
+const makeLi = (idx, text, checked = false) => {
+  const input = document.createElement('input');
+  input.setAttribute('type', 'checkbox');
+  input.setAttribute('id', `item${idx}`);
+  input.setAttribute('data-index', `${idx}`);
+  if (checked) input.setAttribute('checked');
+
+  const label = document.createElement('label');
+  label.setAttribute('for', `item${idx}`);
+  label.textContent = text;
+
+  const li = document.createElement('li');
+  li.append(input, label);
+  return li;
+};
+
+const populateList = (listItems = [], list) => {
+  const renderedLis = [...list.querySelectorAll('input[data-index]')]
+    .map(input => Number(input.dataset.index));
+  list.append(...listItems
+    .filter(({ idx }) => !renderedLis.includes(idx))
+    .map(({ idx, text, done }) => makeLi(idx, text, done)));
+};
+
+const addItem = (e) => {
+  e.preventDefault();
+  const form = e.target;
+  const text = form.querySelector('input[name=item]').value;
+  const { items } = state;
+  state.items = [...items, { idx: items.length, text, done: false }];
+  populateList(state.items, itemsList);
+  form.reset();
+};
+
+addItems.addEventListener('submit', addItem);
