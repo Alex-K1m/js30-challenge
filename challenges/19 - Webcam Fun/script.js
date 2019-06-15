436cd7d593cf3afa74016fda7c8c06641b81c9b7
Alex Kim <henvin.3@outlook.com>
2019-06-15 20:57:04 +0500

    webcam add video streming

--- challenges/19 - Webcam Fun/script.js
+++ challenges/19 - Webcam Fun/script.js
@@ -5,3 +5,27 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+const getVideo = async () => {
+  try {
+    const localMediaStream = await navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false });
+    video.srcObject = localMediaStream;
+    video.play();
+  } catch (err) {
+    console.log('No camera access', err);
+  }
+};
+
+const paintToCanvas = () => {
+  const { videoWidth, videoHeight } = video;
+  canvas.width = videoWidth;
+  canvas.height = videoHeight;
+
+  const render = () => { ctx.drawImage(video, 0, 0, videoWidth, videoHeight); };
+  setInterval(render, 17);
+};
+
+video.addEventListener('canplay', paintToCanvas);
+
+getVideo();
